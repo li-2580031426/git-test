0407cbb5a588e9c0a2c7ca367faf22e3277a5a72
LiJiangTao <2580031426@qq.com>
2020-11-24 11:43:42 +0800

    修过firstclass

--- src/com/itheima/git/firstClass.java
+++ src/com/itheima/git/firstClass.java
@@ -7,9 +7,12 @@ public class firstClass {
         System.out.println("heloo git project");
         System.out.println("heloo git project");
         System.out.println("heloo git project");
+        //hello test
     }
     public void test(){
         int i = 0;
         i+=10;
+        System.out.println(i);
     }
+
 }
